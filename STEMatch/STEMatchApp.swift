6cab3c2f026e31b1e8d4290dbb43d1782810dea9
Sarah Ramzan <sarahramzan1215@gmail.com>
2023-07-17 15:21:57 -0700

    Initial Commit

---
+++ STEMatch/STEMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEMatchApp.swift
+//  STEMatch
+//
+//  Created by Scholar on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct STEMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
